5b3c65cf34721659645f1e4b9e85368c8e81d055
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-06-23 15:07:08 +0100

    SFAT-159 - reverted sg egress CIDR block restrictions

--- terraform/modules/guided-match/main.tf
+++ terraform/modules/guided-match/main.tf
@@ -14,17 +14,17 @@ resource "aws_security_group" "allow_postgres_external" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port = 5432
-    to_port   = 5432
-    protocol  = "tcp"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 5432
-    to_port   = 5432
-    protocol  = "tcp"
-    cidr_blocks = var.cidr_blocks_db
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
